a9c14f63957c529b66e8495e8cf90867dbdac3a1
arman-sh <rosko1245@gmail.com>
2019-05-05 13:53:17 +0300

    v 3.2

--- src/main/java/ru/mail/polis/persistent/CustomDAO.java
+++ src/main/java/ru/mail/polis/persistent/CustomDAO.java
@@ -9,92 +9,89 @@ import ru.mail.polis.Record;
 import java.io.File;
 import java.io.IOException;
 import java.nio.ByteBuffer;
-import java.nio.file.FileVisitResult;
-import java.nio.file.Files;
-import java.nio.file.Path;
-import java.nio.file.SimpleFileVisitor;
-import java.nio.file.attribute.BasicFileAttributes;
+import java.nio.file.*;
 import java.util.ArrayList;
+import java.util.Collection;
 import java.util.Iterator;
 import java.util.List;
 
 public class CustomDAO implements DAO {
 
+    public static final String SUFFIX_DAT = ".dat";
+    public static final String SUFFIX_TMP = ".tmp";
+
+    private final File directory;
+    private final long flushLimit;
     private MemTable memTable;
-    private final File baseDirectory;
-    private List <SSTable> ssTables;
+    private final Collection<Path> files;
+    private int generation;
 
-    public CustomDAO(@NotNull final File baseDirectory, long flushLimit) throws IOException {
-        this.baseDirectory = baseDirectory;
-        this.memTable = new MemTable(this.baseDirectory, flushLimit);
-        this.ssTables = new ArrayList<>();
-        Files.walkFileTree(this.baseDirectory.toPath(), new SimpleFileVisitor<>(){
-            @Override
-            public FileVisitResult visitFile(Path file, BasicFileAttributes attrs) throws IOException {
-                ssTables.add(new SSTable(file.toFile()));
-                return FileVisitResult.CONTINUE;
-            }
-        });
+
+    public CustomDAO(final File directory, final long flushLimit) throws IOException {
+       this.directory = directory;
+       assert flushLimit >= 0L;
+       this.flushLimit = flushLimit;
+       memTable = new MemTable();
+       files = new ArrayList<>();
+       Files.walk(directory.toPath(), 1)
+               .filter(path-> path.getFileName().toString().endsWith(SUFFIX_DAT))
+               .forEach(files::add);
     }
 
-    private Iterator<Record> ssTablesIterator(@NotNull ByteBuffer from) {
-        final List <Iterator<Cluster>> sstablesListIter = new ArrayList<>();
-        for (SSTable ssTable : ssTables) {
-            sstablesListIter.add(ssTable.iterator(from));
-        }
-        final Iterator <Cluster> sstablesIter = Iterators.mergeSorted(sstablesListIter, Cluster.COMPARATOR);
-        final Iterator <Cluster> sstableCollapseIter = Iters.collapseEquals(sstablesIter, Cluster::getKey);
-
-        final Iterator <Cluster> aliveClusters = Iterators.filter(sstableCollapseIter, cluster-> {
-            assert  cluster != null;
-            return !cluster.getClusterValue().isTombstone();
-        });
-        return Iterators.transform(aliveClusters, cluster->{
-            assert  cluster != null;
-            return Record.of(cluster.getKey(), cluster.getClusterValue().getData());
-        });
-    }
-
-    private Iterator<Record> memTableIterator(@NotNull ByteBuffer from) throws IOException {
-        /*
-         * Get Alive Clusters form MemTable
-         * */
-        final Iterator <Cluster> clusters = memTable.iterator(from);
-        final Iterator <Cluster> aliveClusters = Iterators.filter(clusters, cluster -> {
-            assert cluster != null;
-            return !cluster.getClusterValue().isTombstone();
-        });
-        return Iterators.transform(aliveClusters, cluster -> {
-            assert cluster != null;
-            return Record.of(cluster.getKey(), cluster.getClusterValue().getData());
-        });
-    }
 
     @NotNull
     @Override
     public Iterator<Record> iterator(@NotNull ByteBuffer from) throws IOException {
-        final Iterator <Record> memtableIter = memTableIterator(from);
-        final Iterator <Record> sstablesIter = ssTablesIterator(from);
-        final List <Iterator<Record>> iteratorList = new ArrayList<>();
-        iteratorList.add(memtableIter);
-        iteratorList.add(sstablesIter);
-        final Iterator <Record> iterator = Iterators.mergeSorted(iteratorList, Record::compareTo);
-        final Iterator <Record> collapseEqualsIter = Iters.collapseEquals(iterator, Record::getKey);
-        return collapseEqualsIter;
+        final List <Iterator <Cluster>> iters = new ArrayList<>();
+        for(final Path path : this.files) {
+            SSTable ssTable = new SSTable(path.toFile());
+            iters.add(ssTable.iterator(from));
+        }
+
+        iters.add(memTable.iterator(from));
+        final Iterator <Cluster> clusterIterator = Iters.collapseEquals(
+                Iterators.mergeSorted(iters, Cluster.COMPARATOR),
+                Cluster::getKey
+        );
+        final Iterator <Cluster> alive = Iterators.filter(
+                clusterIterator, cluster-> {
+                    assert cluster != null;
+                    return !cluster.getClusterValue().isTombstone();
+                }
+        );
+        return Iterators.transform(alive, cluster-> {
+            assert cluster != null;
+            return Record.of(cluster.getKey(), cluster.getClusterValue().getData());
+        });
     }
 
     @Override
     public void upsert(@NotNull ByteBuffer key, @NotNull ByteBuffer value) throws IOException {
         memTable.upsert(key, value);
+        if(memTable.size() > flushLimit) {
+            flush();
+        }
     }
 
     @Override
     public void remove(@NotNull ByteBuffer key) throws IOException {
         memTable.remove(key);
+        if(memTable.size() > flushLimit) {
+            flush();
+        }
     }
 
     @Override
     public void close() throws IOException {
-        WriteToFileHelper.writeToFile(memTable.iterator(ByteBuffer.allocate(0)), baseDirectory, memTable.getGeneration());
+        flush();
+    }
+
+    private void flush() throws IOException {
+        final File tmp = new File(directory, generation + SUFFIX_TMP);
+        SSTable.writeToFile(memTable.iterator(ByteBuffer.allocate(0)), tmp);
+        final File dest = new File(directory, generation + SUFFIX_DAT);
+        Files.move(tmp.toPath(), dest.toPath(), StandardCopyOption.ATOMIC_MOVE);
+        generation = generation + 1;
+        memTable = new MemTable();
     }
 }
--- src/main/java/ru/mail/polis/persistent/SSTable.java
+++ src/main/java/ru/mail/polis/persistent/SSTable.java
@@ -10,13 +10,68 @@ import java.nio.ByteOrder;
 import java.nio.LongBuffer;
 import java.nio.channels.FileChannel;
 import java.nio.file.StandardOpenOption;
+import java.util.ArrayList;
 import java.util.Iterator;
+import java.util.List;
 
 public class SSTable {
 
     private final int rows;
     private final LongBuffer offsets;
     private final ByteBuffer clusters;
+
+
+    public static void writeToFile(Iterator <Cluster> clusters, File to) throws IOException {
+        try(FileChannel fileChannel = FileChannel.open(
+                to.toPath(), StandardOpenOption.CREATE_NEW, StandardOpenOption.WRITE))
+        {
+            final List<Long> offsets = new ArrayList<>();
+            long offset = 0;
+            while (clusters.hasNext()) {
+                offsets.add(offset);
+
+                final Cluster cluster = clusters.next();
+
+                // Write Key
+                final ByteBuffer key = cluster.getKey();
+                final int keySize = cluster.getKey().remaining();
+                fileChannel.write(BytesWrapper.fromInt(keySize));
+                offset += Integer.BYTES; // 4 byte
+                ByteBuffer keyDuplicate = key.duplicate();
+                fileChannel.write(keyDuplicate);
+                offset += keySize;
+
+                // Value
+                final ClusterValue value = cluster.getClusterValue();
+
+                // Write Timestamp
+                if (value.isTombstone()) {
+                    fileChannel.write(BytesWrapper.fromLong(-cluster.getClusterValue().getTimestamp()));
+                } else {
+                    fileChannel.write(BytesWrapper.fromLong(cluster.getClusterValue().getTimestamp()));
+                }
+                offset += Long.BYTES; // 8 byte
+
+                // Write Value Size and Value
+
+                if (!value.isTombstone()) {
+                    final ByteBuffer valueData = value.getData();
+                    final int valueSize = value.getData().remaining();
+                    fileChannel.write(BytesWrapper.fromInt(valueSize));
+                    offset += Integer.BYTES; // 4 byte
+                    fileChannel.write(valueData);
+                    offset += valueSize;
+                }
+                //else - not write Value
+            }
+            // Write Offsets
+            for (final Long anOffset : offsets) {
+                fileChannel.write(BytesWrapper.fromLong(anOffset));
+            }
+            //Cells
+            fileChannel.write(BytesWrapper.fromLong(offsets.size()));
+        }
+    }
 
 
     public SSTable(final File file) throws IOException {
