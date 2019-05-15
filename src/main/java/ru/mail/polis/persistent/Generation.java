19d7dae6b1babbddc2c997cd68612168fee28ba0
arman-sh <rosko1245@gmail.com>
2019-05-15 18:59:05 +0300

    def style

--- src/main/java/ru/mail/polis/persistent/Generation.java
+++ src/main/java/ru/mail/polis/persistent/Generation.java
@@ -2,11 +2,7 @@ package ru.mail.polis.persistent;
 
 import org.jetbrains.annotations.NotNull;
 
-import java.io.File;
 import java.nio.file.Path;
-import java.util.List;
-import java.util.regex.Matcher;
-import java.util.regex.Pattern;
 
 public final class Generation {
     private Generation(){}
@@ -23,8 +19,8 @@ public final class Generation {
      **/
 
 
-    public static long getNumericValue(@NotNull final String name){
-        StringBuilder res = new StringBuilder();
+    private static long getNumericValue(@NotNull final String name){
+        final StringBuilder res = new StringBuilder();
         final String [] tmp0 = name.split("/");
         final String tmp1 = tmp0[tmp0.length - 1];
         for(int i = 0; i < tmp1.length(); i++) {
